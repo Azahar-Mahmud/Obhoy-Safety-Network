50ac41df262ad05e348d6af48bca28ccab663ffd
Azahar_Mahmud <chowdhuryrafi0211@gmail.com>
2026-07-22 04:31:34 +0600

    Enable CORS for Web Tracker

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const authRoutes = require('./routes/auth');
 const contactsRoutes = require('./routes/contacts');
@@ -11,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({ status: 'ok', message: 'Obhoy backend is running' });
